9e3e7ab7e94d7c630b89e3ec066f71702ee5f5a5
rnktlhr <rinakitluhur10@gmail.com>
2024-04-05 13:52:25 +0700

    menambahkan if else pada while yang ada di void input

--- AlgorithmaSearch_0069/AlgorithmaSearch_0069.cpp
+++ AlgorithmaSearch_0069/AlgorithmaSearch_0069.cpp
@@ -10,7 +10,10 @@ void input(){
 	{
 		cout << "Enter the number of elements in the array: ";
 		cin >> n;
-
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
 }
 
